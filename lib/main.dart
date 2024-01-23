9f31095355538158707c64c046051522f4c1a756
Prihantoedi <prihanto_sanjaya@yahoo.com>
2024-01-23 16:38:48 +0700

    centering the body content

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text('hey ninjas!'),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text("Hello Pri"),
+      ),
+    ),    
   ));
 }
+
+
