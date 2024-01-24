fdcdc0d1d75bfb3e62fcf3a85b8120b989af6091
Prihantoedi <prihanto_sanjaya@yahoo.com>
2024-01-24 12:01:55 +0700

    styling the font get from google fonts download the zip file create a new diretory named fonts paste the extracted zip file of fonts change the pubspec.yaml in font family part add the font family property in the text style

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,24 @@ void main() {
       appBar: AppBar(
         title: Text(
           'my first app',
-          style: TextStyle(color: Colors.white),),
+          style: TextStyle(
+              color: Colors.white
+          ),
+        ),
         backgroundColor: Colors.blue,
         centerTitle: true,
       ),
       body: Center(
-        child: Text("Hello Pri"),
+        child: Text(
+            "Hello Pri",
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 2.0,
+              color: Colors.grey[600],
+              fontFamily: 'IndieFlower',
+            ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: (){},
