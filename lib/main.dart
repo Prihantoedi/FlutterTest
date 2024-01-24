7a74f6d7adcdc8cf4e02176bfdbb7930311dc5d8
Prihantoedi <prihanto_sanjaya@yahoo.com>
2024-01-24 15:02:26 +0700

    add image from url and from local directory

--- lib/main.dart
+++ lib/main.dart
@@ -2,39 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'my first app',
-          style: TextStyle(
-              color: Colors.white
-          ),
-        ),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-            "Hello Pri",
+    home: Home(),
+  ));
+}
+
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'my first app',
             style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
-              fontFamily: 'IndieFlower',
+                color: Colors.white
             ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-          onPressed: (){},
-          child: Text(
-              'Click',
-              style: TextStyle(color: Colors.white),
           ),
           backgroundColor: Colors.blue,
-      )
-    ),    
-  ));
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Image(
+                image: AssetImage('assets/dice.jpg'),
+          ),
+              // image: NetworkImage('https://images.unsplash.com/photo-1489549132488-d00b7eee80f1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D') with url
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: Text(
+            'Click',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.blue,
+        )
+    );
+  }
 }
 
 
